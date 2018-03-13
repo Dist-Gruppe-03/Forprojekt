393dc98786f00569d4266a894dcab1b9c52fac7e
s133996 <mit.navn.marcus@jubii.dk>
2018-03-13 13:49:12 +0100

    DR ordliste igen

--- src/galgeleg/Galgelogik.java
+++ src/galgeleg/Galgelogik.java
@@ -17,6 +17,24 @@ import brugerautorisation.transport.rmi.Brugeradmin;
 import java.rmi.Naming;
 import java.rmi.server.UnicastRemoteObject;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Random;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebService(endpointInterface = "galgeleg.GalgeI")
 public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
@@ -29,6 +47,10 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
   private boolean spilletErVundet;
   private boolean spilletErTabt;
   private Brugeradmin BA;
+  private int scoren;
+  private int[] Highscore = new int[10];
+  private int[] revHighscore = new int[10];
+  private int[] sortedHighscore = new int[10];
   
 
   public ArrayList<String> getBrugteBogstaver() {
@@ -44,6 +66,10 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
     return ordet;
   }
 
+   public int getScoren() {
+    return scoren;
+  }
+   
   public int getAntalForkerteBogstaver() {
     return antalForkerteBogstaver;
   }
@@ -84,6 +110,7 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
     antalForkerteBogstaver = 0;
     spilletErVundet = false;
     spilletErTabt = false;
+    scoren = 0;
     ordet = muligeOrd.get(new Random().nextInt(muligeOrd.size()));
     opdaterSynligtOrd();
   }
@@ -104,6 +131,7 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
   }
 
   public void gætBogstav(String bogstav) {
+      
     if (bogstav.length() != 1) return;
     System.out.println("Der gættes på bogstavet: " + bogstav);
     if (brugteBogstaver.contains(bogstav)) return;
@@ -114,14 +142,17 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
     if (ordet.contains(bogstav)) {
       sidsteBogstavVarKorrekt = true;
       System.out.println("Bogstavet var korrekt: " + bogstav);
+      scoren++;
     } else {
       // Vi gættede på et bogstav der ikke var i ordet.
       sidsteBogstavVarKorrekt = false;
       System.out.println("Bogstavet var IKKE korrekt: " + bogstav);
       antalForkerteBogstaver = antalForkerteBogstaver + 1;
       if (antalForkerteBogstaver > 6) {
+          skrivHighScore();
         spilletErTabt = true;
       }
+      scoren--;
     }
     opdaterSynligtOrd();
   }
@@ -129,14 +160,20 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
   public void logStatus() {
     System.out.println("---------- ");
     System.out.println("- ordet (skult) = " + ordet);
+    System.out.println("- scoren = " + scoren);
     System.out.println("- synligtOrd = " + synligtOrd);
     System.out.println("- forkerteBogstaver = " + antalForkerteBogstaver);
     System.out.println("- brugeBogstaver = " + brugteBogstaver);
-    if (spilletErTabt) System.out.println("- SPILLET ER TABT");
-    if (spilletErVundet) System.out.println("- SPILLET ER VUNDET");
+    if (spilletErTabt){
+        skrivHighScore();
+        System.out.println("- SPILLET ER TABT");
+    }
+    if (spilletErVundet){
+        skrivHighScore();
+        System.out.println("- SPILLET ER VUNDET");
+    }
     System.out.println("---------- ");
   }
-
 
   public boolean hentBruger(String brugernavn, String adgangskode) {
 	  try {
@@ -152,8 +189,50 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
   }
   
  
+  public void skrivHighScore(){
+      
+      laesHighScore();
+      List<String> lines = Arrays.asList(Integer.toString(Highscore[0]),Integer.toString(Highscore[1]),Integer.toString(Highscore[2]),Integer.toString(Highscore[3]),Integer.toString(Highscore[4]),Integer.toString(Highscore[5]),Integer.toString(Highscore[6]),Integer.toString(Highscore[7]),Integer.toString(Highscore[8]),Integer.toString(Highscore[9]));
+        Path file = Paths.get("highscore.txt");
+        
+          try {
+              Files.write(file, lines, Charset.forName("UTF-8"));
+          } catch (IOException ex) {
+              Logger.getLogger(Galgelogik.class.getName()).log(Level.SEVERE, null, ex);
+          }
+  }
+  
+   public int[] laesHighScore() {
+       
+      try {
+          
+          int counter = 0;
+          BufferedReader in = new BufferedReader(new FileReader("highscore.txt"));
+          String line;
+          while((line = in.readLine()) != null){
+              Highscore[counter] = Integer.parseInt(line);
+              counter++;   
+          }
+          
+          in.close();
 
-/*
+          Arrays.sort(Highscore);
+          Highscore[0] = scoren;
+          Arrays.sort(Highscore);
+               
+          return Highscore;
+      } catch (IOException ex) {
+          List<String> lines = Arrays.asList("0");
+          Path file = Paths.get("highscore.txt");
+          try {
+              Files.write(file, lines, Charset.forName("UTF-8"));
+          } catch (IOException ex1) {
+              Logger.getLogger(Galgelogik.class.getName()).log(Level.SEVERE, null, ex1);
+          }
+      }
+  return Highscore;
+   }
+
   public static String hentUrl(String url) throws IOException {
     System.out.println("Henter data fra " + url);
     BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()));
@@ -165,11 +244,16 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
     }
     return sb.toString();
   }
-*/
-/*
-  public void hentOrdFraDr() throws Exception {
-    String data = hentUrl("https://dr.dk");
-    //System.out.println("data = " + data);
+
+  public void hentOrdFraDr() throws RuntimeException {
+    String data = null;
+      try {
+          data = hentUrl("https://dr.dk");
+          //System.out.println("data = " + data);
+      } catch (IOException ex) {
+          Logger.getLogger(Galgelogik.class.getName()).log(Level.SEVERE, null, ex);
+      }
+
     data = data.substring(data.indexOf("<body")). // fjern headere
             replaceAll("<.+?>", " ").toLowerCase(). // fjern tags
             replaceAll("&#198;", "æ"). // erstat HTML-tegn
@@ -181,12 +265,13 @@ public class Galgelogik  extends UnicastRemoteObject implements GalgeI {
             replaceAll("[^a-zæøå]", " "). // fjern tegn der ikke er bogstaver
             replaceAll(" [a-zæøå] "," "). // fjern 1-bogstavsord
             replaceAll(" [a-zæøå][a-zæøå] "," "); // fjern 2-bogstavsord
+
     System.out.println("data = " + data);
     System.out.println("data = " + Arrays.asList(data.split("\\s+")));
     muligeOrd.clear();
     muligeOrd.addAll(new HashSet<String>(Arrays.asList(data.split(" "))));
+
     System.out.println("muligeOrd = " + muligeOrd);
     nulstil();
   }
-*/
 }
