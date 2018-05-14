50d3645b7ab8c2591d1f77b5cdde07d8a700cd11
KredeDK <krede92@gmail.com>
2018-05-14 20:16:20 +0200

    Updated link Made galgeserver.java main class

--- src/galgeleg/Galgeserver.java
+++ src/galgeleg/Galgeserver.java
@@ -11,11 +11,11 @@ import javax.xml.ws.Endpoint;
 public class Galgeserver {
 
     public static void main(String[] arg) throws Exception {
-        java.rmi.registry.LocateRegistry.createRegistry(9923); // start i server-JVM
+        java.rmi.registry.LocateRegistry.createRegistry(9943); // start i server-JVM
 
         GalgeI g = new Galgelogik();
-        Naming.rebind("rmi://[::]:9923/galgeleg", g);
-        Endpoint.publish("http://[::]:9924/galgeleg", g);
+        Naming.rebind("rmi://[::]:9943/galgeleg", g);
+        Endpoint.publish("http://[::]:9944/galgeleg", g);
         System.out.println("Galgeleg startet.");
 
     }
